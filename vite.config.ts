060d518f5a805016d3d159812c789c35a44beaaa
Threona Huynh <66297538+Threona-Kannard@users.noreply.github.com>
2025-10-08 02:20:28 +0700

    Update Code base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
