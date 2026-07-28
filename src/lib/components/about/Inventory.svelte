18fd15a7cb8efb5329ce0152cc5e5f540be06f94
Threona Huynh <phattanhuynh1324@gmail.com>
2026-07-29 00:25:37 +0700

    Update inventory

--- src/lib/components/about/Inventory.svelte
+++ src/lib/components/about/Inventory.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { goto } from "$app/navigation";
     import CameraImg from "$lib/assets/images/inventory/camera.webp?enhanced";
     import PhoneImg from "$lib/assets/images/inventory/mobile.webp?enhanced";
     import IpodImg from "$lib/assets/images/inventory/ipod-mini.webp?enhanced";
@@ -6,33 +7,33 @@
 </script>
 
 <div class="about-inventory">
-    <div class="inventory-item">
+    <button class="inventory-item" onclick={() => goto("/galleries")}>
         <div>
             <enhanced:img src={CameraImg} alt="" loading="lazy"> </enhanced:img>
         </div>
         <p>Canon A3100 IS</p>
-    </div>
+    </button>
 
-    <div class="inventory-item">
+    <button class="inventory-item" onclick={() => goto("/music")}>
         <div>
             <enhanced:img src={PhoneImg} alt="" loading="lazy"> </enhanced:img>
         </div>
         <p>Samsung S23 Ultra</p>
-    </div>
+    </button>
 
-    <div class="inventory-item">
+    <button class="inventory-item" onclick={() => goto("/games")}>
         <div>
             <enhanced:img src={IpodImg} alt="" loading="lazy"> </enhanced:img>
         </div>
         <p>ROG Ally</p>
-    </div>
+    </button>
 
-    <div class="inventory-item">
+    <button class="inventory-item" onclick={() => goto("game_retro")}>
         <div>
             <enhanced:img src={DapImg} alt="" loading="lazy"> </enhanced:img>
         </div>
         <p>Anbernic RG353PS</p>
-    </div>
+    </button>
 </div>
 
 <style>
